3258b1811c9bafb98f2433fea8573bd46eb75fdc
unknown <shitianheshu13@gmail.com>
2026-03-27 14:44:03 +0900

    'update'

--- main.js
+++ main.js
@@ -1039,11 +1039,14 @@ async function scrapeWebsite(sessionId, targetWindow, data) {
         await waitForClickableElement(convenienceStoreSelector, sessionId);
         await directClick(convenienceStoreSelector, 0, sessionId, page);
         await page.waitForTimeout(1000);
+          
+         const locationButton = await page.$(locationEditSelector);
 
-        const locationEditSelector = '.rs-edit-location-button';
-        await waitForClickableElement(locationEditSelector, sessionId);
-        await page.waitForTimeout(300);
-        await directClick(locationEditSelector, 0, sessionId, page);
+        if (locationButton) {
+          await waitForClickableElement(locationEditSelector, sessionId);
+          await page.waitForTimeout(300);
+          await directClick(locationEditSelector, 0, sessionId, page);
+        }
 
         const storeLocatorSearchInputSelector = '[id="checkout.fulfillment.pickupTab.pickup.storeLocator.searchInput"]';
         await page.click(storeLocatorSearchInputSelector, { clickCount: 3 });
